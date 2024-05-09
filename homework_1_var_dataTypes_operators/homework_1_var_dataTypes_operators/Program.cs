d1f4f1cfcd8b01b42c789d3fb5e95dce7829ebdc
Mirzaagha Abdullayev <mirzaaghaaa@code.edu.az>
2024-05-09 12:38:41 +0400

    homework 1 added

---
+++ homework_1_var_dataTypes_operators/homework_1_var_dataTypes_operators/Program.cs
@@ -0,0 +1,55 @@
+﻿
+#region Task1
+
+/*namespace homework_1_var_dataTypes_operators
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                if(i%3==0 && i%5==0)
+                {
+                    Console.WriteLine(i+" is divisible by 3 and 5");
+                }
+            }
+        }
+    }
+}*/
+
+#endregion
+
+#region Task2
+
+/*using System.Text;
+
+namespace homework_1_var_dataTypes_operators
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //for Az letters
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.InputEncoding = Encoding.UTF8;
+
+            int score = -15;
+            if (score < 65 && score>=0)
+                Console.WriteLine("Kəsildin");
+            else if (score >= 65 && score < 85)
+                Console.WriteLine("Adi Diplom");
+            else if (score >= 85 && score <= 95)
+                Console.WriteLine("Şərəf Diplomu");
+            else if (score>95 && score<=100)
+                Console.WriteLine("Yüksək Şərəf Diplomu");
+            else
+                Console.WriteLine("Ədəd 0 və 100 arası olmalıdır");
+        }
+    }
+}*/
+
+#endregion
+
+
+

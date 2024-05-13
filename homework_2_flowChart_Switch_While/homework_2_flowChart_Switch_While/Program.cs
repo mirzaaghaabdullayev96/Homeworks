6ebd03b38dcc4df8293d6bc0b7c2445d48d613bc
Mirzaagha Abdullayev <mirzaaghaaa@code.edu.az>
2024-05-13 21:18:42 +0400

    new homework

---
+++ homework_2_flowChart_Switch_While/homework_2_flowChart_Switch_While/Program.cs
@@ -0,0 +1,57 @@
+﻿namespace homework_2_flowChart_Switch_While
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            #region task_1 
+
+            //1. Verilmiş n tam ədədinin sadə və ya mürəkkəb ədəd olduğunu tapan alqoritm.
+
+            /*int input = Convert.ToInt32(Console.ReadLine());
+
+            int i = 2;
+
+            if (input <= 1)
+            {
+                Console.WriteLine("input must be more than 1");
+            }
+            else
+            {
+                while (i <= input - 1)
+                {
+                    if (input % i == 0)
+                    {
+                        Console.WriteLine($"{input} is composite number");
+                        break;
+                    }
+                    i++;
+                }
+                if (i == input)
+                    Console.WriteLine($"{input} is prime number");
+            }*/
+            #endregion
+
+
+            #region task_2 
+
+            /*//2.Verilmiş n tam ədədinin neçə mərtəbəli olduğunu tapın.Məs: 23452, output: 5 , Məs: 456, output: 3
+
+            int input = Convert.ToInt32(Console.ReadLine());
+
+            int i = 0;
+
+            while (input > 0)
+            {
+                input /= 10;
+                i++;
+            }
+
+            Console.WriteLine(i);*/
+
+            #endregion
+
+        }
+    }
+}

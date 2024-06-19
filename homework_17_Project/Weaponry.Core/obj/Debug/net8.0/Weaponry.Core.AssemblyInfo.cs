49252de12fcca5652cd6928fd3b74edf39f6b59c
Mirzaagha Abdullayev <mirzaaghaaa@code.edu.az>
2024-06-19 17:32:07 +0400

    homework 17 project

--- homework_17_Project/Weaponry.Core/obj/Debug/net8.0/Weaponry.Core.AssemblyInfo.cs
+++ homework_17_Project/Weaponry.Core/obj/Debug/net8.0/Weaponry.Core.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Weaponry.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b67613fd0243bd9ac9161e6fed318a2af4b0edee")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+44d0bbecec238b7bb20237ed7392b5eb7afc8979")]
 [assembly: System.Reflection.AssemblyProductAttribute("Weaponry.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Weaponry.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

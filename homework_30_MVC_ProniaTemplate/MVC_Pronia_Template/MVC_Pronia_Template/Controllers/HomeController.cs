f11bbf3d30e1b7740c5fccf8339b8236c3ed09d6
mirzaabdullayev96 <mirzaabdullayev96@gmail.com>
2024-08-13 23:59:39 +0400

    homework 30 MVC

---
+++ homework_30_MVC_ProniaTemplate/MVC_Pronia_Template/MVC_Pronia_Template/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVC_Pronia_Template.Models;
+using MVC_Pronia_Template.ViewModels;
+
+namespace MVC_Pronia_Template.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Slide> slides =
+            [
+                new() {
+                    Id = 1,
+                    Title = "Home",
+                    SubTitle = "Endirim var",
+                    Order = 2,
+                    Image="1-1.png"
+                },
+                new() {
+                    Id = 2,
+                    Title = "Home 2",
+                    SubTitle = "Endirim var helede",
+                    Order = 3,
+                    Image="1-2.png"
+                },
+                new ()
+                {
+                    Id = 3,
+                    Title = "Home 3",
+                    SubTitle = "Endirim var sabaha geder",
+                    Order = 1,
+                    Image="1-3.png"
+                },
+            ];
+
+            HomeVM homeVM = new()
+            {
+                Slides = slides.OrderBy(s => s.Order).ToList()
+            };
+
+            return View(homeVM);
+        }
+    }
+}
---
+++ homework_30_MVC_ProniaTemplate/MVC_Pronia_Template/MVC_Pronia_Template/ViewModels/HomeVM.cs
@@ -0,0 +1,9 @@
+﻿using MVC_Pronia_Template.Models;
+
+namespace MVC_Pronia_Template.ViewModels
+{
+    public class HomeVM
+    {
+        public List<Slide> Slides { get; set; }
+    }
+}

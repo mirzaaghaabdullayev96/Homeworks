95b306b82027734c8a78aadccde43b64e9dea58e
mirzaabdullayev96 <mirzaabdullayev96@gmail.com>
2024-08-15 11:05:33 +0400

    homework 31 mvc DI

---
+++ homework_31_MVC_DI/MVC_DI/MVC_Pronia_Template/Controllers/ProductController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVC_Pronia_Template.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
